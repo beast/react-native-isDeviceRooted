858ceb45edc7d9b84bdcf03502c4b10781a1b896
yang yao <yang@livepay.my>
2016-09-15 17:14:44 +0800

    bump version to 0.1.1, edit pod file and install uidevice-passcodestatus

--- ios/RNIsDeviceRooted.h
+++ ios/RNIsDeviceRooted.h
@@ -1,6 +1,15 @@
 
 #import "RCTBridgeModule.h"
 
+typedef NS_ENUM(NSUInteger, LNPasscodeStatus){
+    /* The passcode status was unknown */
+    LNPasscodeStatusUnknown   = 0,
+    /* The passcode is enabled */
+    LNPasscodeStatusEnabled   = 1,
+    /* The passcode is disabled */
+    LNPasscodeStatusDisabled  = 2
+};
+
 @interface RNIsDeviceRooted : NSObject <RCTBridgeModule>
 
 @end
--- ios/RNIsDeviceRooted.m
+++ ios/RNIsDeviceRooted.m
@@ -1,13 +1,31 @@
 
 #import "RNIsDeviceRooted.h"
+#import "UIDevice+PasscodeStatus.h"
 
 @implementation RNIsDeviceRooted
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(RNIsDeviceRooted)
 
+RCT_EXPORT_METHOD(isDeviceRooted:(RCTResponseSenderBlock)callback)
+{
+    // todo: import UIdevice passcodestatus then call the method to verify and return the callback to javascript
+    LNPasscodeStatus status = [UIDevice currentDevice].passcodeStatus;
+    
+    switch (status) {
+        case LNPasscodeStatusEnabled:
+            callback(@[[NSNull null], true]);
+            break;
+            
+        case LNPasscodeStatusDisabled:
+            callback(@[[NSNull null], false]);
+            break;
+            
+        case LNPasscodeStatusUnknown:
+        default:
+            callback(@[[NSString @"Unable to retrieve the status of passcode."], null]);
+            break;
+    }
+    
+}
 @end
   
