ac141b9288df7c8c449ae7c3b3cac3e853c7d69e
yang yao <yang@livepay.my>
2016-09-19 18:29:03 +0800

    update static library

---
+++ ios/ReactNativeIsDeviceRooted/ReactNativeIsDeviceRooted.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeIsDeviceRooted.h
+//  ReactNativeIsDeviceRooted
+//
+//  Created by yang yao on 19/09/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactNativeIsDeviceRooted : NSObject
+
+@end
---
+++ ios/ReactNativeIsDeviceRooted/ReactNativeIsDeviceRooted.m
@@ -0,0 +1,13 @@
+//
+//  ReactNativeIsDeviceRooted.m
+//  ReactNativeIsDeviceRooted
+//
+//  Created by yang yao on 19/09/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "ReactNativeIsDeviceRooted.h"
+
+@implementation ReactNativeIsDeviceRooted
+
+@end
